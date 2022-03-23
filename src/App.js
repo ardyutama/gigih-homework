503fa56db686c15e324c1be31b88566ba1b2ec42
ardy.putrautama53 <ardy.putrautama53@gmail.com>
2022-03-23 16:21:44 +0700

    Add Album Cover, Title, and Name

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./styles.css";
+import data from "./data";
+export default function App() {
+    return (
+        <div className="App">
+            <img
+                src={data.album.images[0].url}
+                height={data.album.images[0].height}
+                width={data.album.images[0].width}
+                // width="640"
+                alt="cover album"
+            />
+            <h4>{data.album.name} </h4>
+            <p>{data.artists[0].name}</p>
+            <button>Select</button>
+        </div>
+    );
 }
-
-export default App;
